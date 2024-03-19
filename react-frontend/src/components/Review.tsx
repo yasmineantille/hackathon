76bad7fb838e741a3776a5e9a94d3ae094f3c6c1
yasmineantille <yasmine.antille@gmail.com>
2024-03-20 00:04:29 +0100

    + adds markdown view to additional remarks

--- react-frontend/src/components/Review.tsx
+++ react-frontend/src/components/Review.tsx
@@ -3,24 +3,15 @@ import DiffViewer from "./DiffViewer.tsx";
 export interface ReviewProps {
     originalCode: string;
     reviewedCode: string;
-    additionalComments: string;
 }
 
-const Review = ({originalCode, reviewedCode, additionalComments}: ReviewProps) => {
-    const commentsStyle = {
-        paddingTop: '1rem',
-        width: '90%',
-
-    }
+const Review = ({originalCode, reviewedCode}: ReviewProps) => {
     return (
         <>
             <DiffViewer
                 original={originalCode}
                 modified={reviewedCode}
             ></DiffViewer>
-            <div style={commentsStyle}>
-                {additionalComments}
-            </div>
         </>);
 }
 
