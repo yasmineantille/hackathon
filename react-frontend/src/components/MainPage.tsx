6722518e604a3f3a65a07fb7f649e798a0b27bb9
Cedrik Schimschar <c.schimschar@fqsmedia.de>
2024-03-19 15:17:14 +0100

    Remove language buttons

--- react-frontend/src/components/MainPage.tsx
+++ react-frontend/src/components/MainPage.tsx
@@ -60,11 +60,6 @@ export default function MainPage() {
         }
     }
 
-
-    const changeLanguage = (newLanguage: string) => {
-        setLanguage(newLanguage); // A function provided by the parent to update the language state
-    };
-
     const handleFileUploaded = (content: string | undefined | null) => {
         if (content) {
             setCode(content);
@@ -126,10 +121,6 @@ export default function MainPage() {
                 onChange={handleOnCodeChange}
             ></CodeInput>
             <FileUploader handleFileUploaded={handleFileUploaded}/>
-            <div>
-                <button onClick={() => changeLanguage('javascript')}>JS</button>
-                <button onClick={() => changeLanguage('python')}>Python</button>
-            </div>
             <br/>
             <Button onClick={handleOnPreview}>Preview</Button>
             <br/>
