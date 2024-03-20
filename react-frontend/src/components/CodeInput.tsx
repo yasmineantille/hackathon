53fc70ca3acba3ece155bd3ef09037037f135445
Dan Olaru <dan.olaru@zuehlke.com>
2024-03-20 11:38:36 +0100

    reformat and add detected language indicator to editor

--- react-frontend/src/components/CodeInput.tsx
+++ react-frontend/src/components/CodeInput.tsx
@@ -2,47 +2,50 @@ import MonacoEditor, {OnMount} from '@monaco-editor/react';
 import debounce from 'lodash/debounce';
 
 export interface CodeInputProps {
-    code: string;
-    language: string;
-    onChange: (newValue?: string) => void;
-    onSelectionChange?: (newSelection: string) => void;
-    returnEditorReference: (ref: any) => void;
+  code: string;
+  language: string;
+  onChange: (newValue?: string) => void;
+  onSelectionChange?: (newSelection: string) => void;
+  returnEditorReference: (ref: any) => void;
 }
 
 const CodeInput = ({code, language, onChange, onSelectionChange, returnEditorReference}: CodeInputProps) => {
-    const editorDidMount: OnMount = (editorInstance) => {
-        editorInstance.focus();
-        returnEditorReference(editorInstance);
-        const debouncedSelectionChange = debounce((selection: any) => {
-            const model = editorInstance.getModel();
-            if (model && onSelectionChange) {
-                const selectedText = model.getValueInRange(selection);
-                if (selectedText.trim().length > 0) {
-                    onSelectionChange(selectedText);
-                }
-            }
-        }, 300); // Debounce delay of 300 milliseconds
+  const editorDidMount: OnMount = (editorInstance) => {
+    editorInstance.focus();
+    returnEditorReference(editorInstance);
+    const debouncedSelectionChange = debounce((selection: any) => {
+      const model = editorInstance.getModel();
+      if (model && onSelectionChange) {
+        const selectedText = model.getValueInRange(selection);
+        if (selectedText.trim().length > 0) {
+          onSelectionChange(selectedText);
+        }
+      }
+    }, 300); // Debounce delay of 300 milliseconds
 
-        editorInstance.onDidChangeCursorSelection(({selection}) => {
-            debouncedSelectionChange(selection);
-        });
-    };
+    editorInstance.onDidChangeCursorSelection(({selection}) => {
+      debouncedSelectionChange(selection);
+    });
+  };
 
-    return (
-        <MonacoEditor
-            height="20rem"
-            width="90%"
-            language={language}
-            theme="vs-dark"
-            value={code}
-            options={{
-                selectOnLineNumbers: true,
-            }}
-            onChange={onChange}
-            onMount={editorDidMount}
+  return (
+    <div style={{width: '100%', display: 'fl4x', flexDirection: 'column', alignItems: 'center'}}>
+      <MonacoEditor
+        height="20rem"
+        width="90%"
+        language={language}
+        theme="vs-dark"
+        value={code}
+        options={{
+          selectOnLineNumbers: true,
+        }}
+        onChange={onChange}
+        onMount={editorDidMount}
+      />
 
-        />
-    );
+      <span>Detected language: {language}</span>
+    </div>
+  );
 };
 
 export default CodeInput;
